27554584d3361fb251a42c59b8207461189d5f1f
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2024-07-15 20:36:32 -0300

    Create mitm_debug.py

---
+++ mitm_debug.py
@@ -0,0 +1,21 @@
+def response(flow):
+    print("")
+    print("="*50)
+    #print("FOR: " + flow.request.url)
+    print(flow.request.method + " " + flow.request.path + " " + flow.request.http_version)
+
+    print("-"*50 + "request headers:")
+    for k, v in flow.request.headers.items():
+        print("%-20s: %s" % (k.upper(), v))
+
+    print("-"*50 + "response headers:")
+    for k, v in flow.response.headers.items():
+        print("%-20s: %s" % (k.upper(), v))
+        print("-"*50 + "request headers:")
+
+    # body request
+    print("-"*50 + "request body:")
+    print(flow.request.content)
+    # body response
+    print("-"*50 + "response body:")
+    print(flow.response.content)
